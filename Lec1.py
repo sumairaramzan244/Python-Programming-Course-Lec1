14d04c2ecdebc87978617764217d82ca088e114e
Sumaira Ramzan <sumaira.ramzan244@gmail.com>
2026-05-07 15:27:48 +0500

    Add files via upload

---
+++ Lec1.py
@@ -0,0 +1,32 @@
+# Punjab Hunarmand Program - Batch 3
+# Lecture 1: Python Print Function
+
+# The print() function displays output on the screen
+# We can use double quotes ("") or single quotes ('') - both work the same way
+
+# --- Basic Print Examples ---
+print("Starting to code in Python at Punjab Hunarmand Batch")
+print("I am learning Python programming language at Punjab Hunarmand Batch 3")
+
+# --- Single quotes work the same way ---
+print('Python is a very easy and powerful language!')
+
+# --- Print your name ---
+print("My Name is Sumaira ")
+print("Learning python programming course at Punjab Hunarmand - Batch 3")
+
+# --- Numbers can also be printed ---
+print("Lecture Number:", 1)
+print("Batch Number:", 3)
+
+# --- Print an empty line ---
+print()  # this prints a blank line
+
+# --- Print multiple things at once ---
+print("Punjab", "Hunarmand", "Batch 3")  # separate with a comma
+
+# --- Math calculations inside print ---
+print("2 + 2 =", 2 + 2)
+print("5 x 3 =", 5 * 3)
+
+# Lecture 1 Complete!
